a7fc2b923b5ed41fe633b03e7452b9dd1929d7c7
sgendaneza-web <s.gendaneza@alustudent.com>
2026-04-01 16:45:07 +0200

    Commit 2

--- organizer.sh
+++ organizer.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# create archive folder if it doesn't exist
+# create an archive folder if it doesn't exist
 mkdir -p archive
-
-# create timestamp
+# create a timestamp
 timestamp=$(date +"%Y%m%d-%H%M%S")
 
-# rename file
+# rename the file
 new_name="grades_$timestamp.csv"
-
-# move file to archive
 mv grades.csv archive/$new_name
 
-# create new empty grades.csv
+# created a new empty grades.csv
 touch grades.csv
 
 # log the action
